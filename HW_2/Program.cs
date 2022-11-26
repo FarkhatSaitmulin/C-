c1c378450b387b40f4c2cdc03244be1ac6337087
Frenk KAZ <farhats81@mail.ru>
2022-11-26 20:26:30 +0600

    Сделали две задачки

--- HW_2/Program.cs
+++ HW_2/Program.cs
@@ -3,7 +3,21 @@
 //int RandomDigit = new Random().Next(100,999); // случайное число от 100 до 999
 //Console.WriteLine("Случайное 3-х значное число:" +RandomDigit); // вывод на консоль случайного 3-х значного числа
 Console.Clear();
-int random = new Random().Next(100,999);
+int random = new Random().Next(100,1000);
 int result = (random / 10) % 10;
 Console.WriteLine(result);
 Console.WriteLine(random + " -> "+ result);
+
+ // Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+ Console.Clear();
+ Console.Write("Введите число =  " );
+ int number = int.Parse(Console.ReadLine());
+ if (number<100) Console.WriteLine("Третьего числа нет");
+ else
+ {
+    while (number>1000) number = number/10;
+    Console.WriteLine(number%10);
+ }
+
+ /// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+ /// и проверяет, является ли этот день выходным.
