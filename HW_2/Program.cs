fcbc2497e4cef331ad95504e8c58758965064195
Frenk KAZ <farhats81@mail.ru>
2022-11-29 02:23:12 +0600

    Нчали третье дз

--- HW_2/Program.cs
+++ HW_2/Program.cs
@@ -21,9 +21,11 @@ Console.WriteLine(random + " -> "+ result);
 
  /// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
  /// и проверяет, является ли этот день выходным.
+ 
 Console.Clear();
 Console.WriteLine("Введите день недели от 1 до 7" );
 int day =  int.Parse(Console.ReadLine());
-if (( day < 5 ) && ( day > 0 )) Console.WriteLine("Данный день недели не является выходным днем");
+if (( day < 6 ) && ( day > 0 )) Console.WriteLine("Данный день недели не является выходным днем");
 else 
-if (day == 6  ||  day == 7) Console.WriteLine("Данный день недели является выходным днем");
+    if (day == 6  ||  day == 7) Console.WriteLine("Данный день недели является выходным днем");
+else Console.WriteLine ("Нет такого дня в недели ");
--- HW_3/Program.cs
+++ HW_3/Program.cs
@@ -1,9 +1,28 @@
-﻿/// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
-/// и проверяет, является ли этот день выходным.
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между
+// ними в 3D пространстве.A (3,6,8); B (2,1,-7), -> 15.84, A (7,-5, 0); B (1,-1,9) -> 11.53
 Console.Clear();
-Console.WriteLine("Введите день недели от 1 до 7" );
-int day =  int.Parse(Console.ReadLine());
-if (( day < 6 ) && ( day > 0 )) Console.WriteLine("Данный день недели не является выходным днем");
-else 
-if (day == 6  ||  day == 7) Console.WriteLine("Данный день недели является выходным днем");
-else Console.WriteLine ("Нет такого дня в недели ");
+int DataEntryXY (string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+double RASTOYNIE (int x1, int x2,int y1,int y2)
+{
+    // return Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    double rast = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    return rast;
+}
+
+int x1 = DataEntryXY ("Введите координаты x1 = ");
+// Console.WriteLine(x);
+int y1 = DataEntryXY ("Введите координаты y1 = ");
+// Console.WriteLine(y);
+int x2 = DataEntryXY ("Введите координаты x2 = ");
+// Console.WriteLine(x);
+int y2 = DataEntryXY ("Введите координаты y2 = ");
+
+double tochki = RASTOYNIE(x1,x2,y1, y2);
+
+
+Console.WriteLine (tochki);
