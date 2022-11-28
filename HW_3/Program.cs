351ca4bd60a9da029ec71bb944a6c3c4ecce7c7b
Frenk KAZ <farhats81@mail.ru>
2022-11-28 20:19:40 +0600

    Новое решение

--- HW_3/Program.cs
+++ HW_3/Program.cs
@@ -6,3 +6,4 @@ int day =  int.Parse(Console.ReadLine());
 if (( day < 6 ) && ( day > 0 )) Console.WriteLine("Данный день недели не является выходным днем");
 else 
 if (day == 6  ||  day == 7) Console.WriteLine("Данный день недели является выходным днем");
+else Console.WriteLine ("Нет такого дня в недели ");
