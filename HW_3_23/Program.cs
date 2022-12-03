ec4a22bd86603d6d2a6ce2c5bbf770a67442db37
Frenk KAZ <farhats81@mail.ru>
2022-12-03 15:42:27 +0600

    Еще одно решение задачки 19

--- HW_3_23/Program.cs
+++ HW_3_23/Program.cs
@@ -11,12 +11,12 @@ int number = int.Parse(Console.ReadLine());
 void Polindrome(string str)
 {
     Console.WriteLine(str);
-    int ostatok = number/1000;
-    int ostatok1 = number%100;
-    int ostatok2 = (ostatok1 % 10) * 10 + (ostatok1/10);
-    if (ostatok == ostatok2) Console.WriteLine ($"число {number} является полиндромом ");
-    else Console.WriteLine ($"число {number} не является полиндромом ");
+    int ostatok = number / 1000;
+    int ostatok1 = number % 100;
+    int ostatok2 = (ostatok1 % 10) * 10 + (ostatok1 / 10);
+    if (ostatok == ostatok2) Console.WriteLine($"число {number} является полиндромом ");
+    else Console.WriteLine($"число {number} не является полиндромом ");
     Console.WriteLine();
 }
 
-Polindrome ("Проверяем является ли число полиндромным: ");
+Polindrome("Проверяем является ли число полиндромным: ");
---
+++ HW_3_23_19/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет, 12821 -> да,  23432 -> да.
+
+Console.Clear();
+void DataEntry(string str)
+{
+    Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+    int Reverse = 0;
+    int numberData = number;
+    while (number < 0)
+    {
+        int remainder = number % 10;
+        Reverse = (Reverse * 10) + remainder;
+        number = number / 10;
+    }
+    if (numberData == Reverse)
+    {
+        Console.WriteLine($"{numberData}--> является полиндромным числом");
+    }
+    else
+    {
+        Console.WriteLine($"{numberData}--> не является полиндромным числом");
+    }
+
+}
+DataEntry("Введите пятизначное число ");
