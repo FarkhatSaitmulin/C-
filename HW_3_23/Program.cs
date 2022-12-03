5ee7d142d7a3f8a56ff4d3179ca64e9cd1b8274a
Frenk KAZ <farhats81@mail.ru>
2022-12-03 14:13:00 +0600

    Корректировка задач

--- HW_3_23/Program.cs
+++ HW_3_23/Program.cs
@@ -1,19 +1,19 @@
-﻿// Задача 23
-// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-// 3 -> 1, 8, 27; 5 -> 1, 8, 27, 64, 12
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет, 12821 -> да,  23432 -> да.
 
 void TablePOW2(string str)
 {
     Console.WriteLine(str);
-    int number = int.Parse(Console.ReadLine()); 
-    int count =1;
-    while (count<= number)
+    int number = int.Parse(Console.ReadLine());
+    int count = 1;
+    while (count <= number)
     {
-        int pow = count*count;
+        int pow = count * count;
         Console.Write(pow + " ");
         count++;
     }
     Console.WriteLine();
 }
 Console.Clear();
-TablePOW2 ("Введите число N: ");
+TablePOW2("Введите число N: ");
