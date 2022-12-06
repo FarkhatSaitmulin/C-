f3e513d6f2957eb64e1d1e8c873cb3d025ef8a19
Frenk KAZ <farhats81@mail.ru>
2022-12-07 03:15:26 +0600

    решаю задачу с массивами

---
+++ HW_4_27/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// NUMBER %10     2///5///4
+//NUMBER /10  45///4///0
+//WHILE(NUM>0) 
+
+
+Console.Clear();
+int DataInput(string str)
+{
+Console.WriteLine(str);
+int number = int.Parse(Console.ReadLine());
+return number;
+}
+int SumNumbers(int num) 
+{ 
+    int result = 0; 
+    while(num > 0) 
+    { 
+        result += num % 10; 
+        num = num / 10;         
+    } 
+    return result; 
+} 
+ 
+Console.Clear(); 
+ 
+int number = DataInput("Многоуважаемый гражданин введите число "); 
+int res = SumNumbers(number); 
+ 
+Console.WriteLine($"{number} -> {res}");
---
+++ HW_4_29/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int DataInput (string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int []FillArray(int [] array)
+{
+    int Length = array.Length;
+    for ( i = 0; i < Length; i++); 
+    {
+        array[i] = newRandom().Next(0,7);
+    }
+    return array;
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int i;
+    for (i = 0; i < count; i++)
+    {
+        Console.Write(col[i] + " ");
+    }
+}
+
+int NumberN = DataInput("Введите число: ");
+int [] array = new int[NumberN];
+array = FillArray(array);
+PrintArray(array);
